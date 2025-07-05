f990ad8a7888ab7897b5fa09ebd60cec41e064af
excloud <andiansyah0425@gmail.com>
2025-07-05 15:37:01 +0700

    create tags model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,14 +7,13 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
+use function Illuminate\Log\log;
+
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        User::factory(1000)->create();
+        User::factory(100)->create();
 
         User::factory()->create([
             'name' => 'Andiansyah',
@@ -22,6 +21,12 @@ class DatabaseSeeder extends Seeder
             'email' => 'andiansyah@kdua.net',
             'password' => Hash::make('Semangatku@25'),
             'avatar' => '/assets/default.jpg',
+        ]);
+
+        log()->info('test');
+
+        $this->call([
+            TagSeeder::class,
         ]);
     }
 }
